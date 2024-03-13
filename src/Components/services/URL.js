767762bad904be0e8ac4868af94bb6eac3e5c1ba
damcode1990 <damcode1990@gmail.com>
2024-03-13 15:17:41 +0530

    term and condision add 1

--- src/Components/services/URL.js
+++ src/Components/services/URL.js
@@ -1,2 +1,2 @@
-// export const baseURL = `https://talkangel-api.onrender.com/api/v1/`;
-export const baseURL = `http://localhost:8000/api/v1/`;
+export const baseURL = `https://talkangel-api.onrender.com/api/v1/`;
+// export const baseURL = `http://localhost:8000/api/v1/`;
