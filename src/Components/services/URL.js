73ab547287cc5428ab70ff23538fe5756d56a25b
damcode1990 <damcode1990@gmail.com>
2024-03-15 11:08:12 +0530

    update new url 1

--- src/Components/services/URL.js
+++ src/Components/services/URL.js
@@ -1,2 +1,2 @@
-export const baseURL = `https://talkangels-api.vercel.app/api/v1/`;
+export const baseURL = `https://talkangels-api.onrender.com/api/v1/`;
 // export const baseURL = `http://localhost:8000/api/v1/`;
