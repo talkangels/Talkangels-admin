68ee0b9eee461bd780275594dd4775bbcbdde2ba
talkangels <155733941+talkangels@users.noreply.github.com>
2024-09-13 19:00:34 +0530

    Update routing.js

--- src/utils/routing.js
+++ src/utils/routing.js
@@ -6,7 +6,7 @@ export const Routing = {
   Register: "/admin/register",
   PrivacyPolicy: "/policies",
   TermsConditions: "/tnc",
-  Payment:"/payment",
+   Payment:"/payment/:id",
   Faq: "/faq's",
   About: "/contact-us",
   Listeners: "/listeners",
