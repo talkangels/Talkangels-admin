259223e9f4245ce9f57d615cc71d584ab822a67c
talkangels <155733941+talkangels@users.noreply.github.com>
2024-09-30 15:57:45 +0530

    Update URL.js

--- src/Components/services/URL.js
+++ src/Components/services/URL.js
@@ -1,3 +1,3 @@
- export const baseURL = `https://talkangels-api.onrender.com/api/v1/`;
-// export const baseURL = `https://web.talkangels.com/api/v1/`;
+ // export const baseURL = `https://talkangels-api.onrender.com/api/v1/`;
+export const baseURL = `https://web.talkangels.com/api/v1/`;
 // export const baseURL = `http://localhost:8000/api/v1/`;
