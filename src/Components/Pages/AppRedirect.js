f9d92a9253dbaba14b01764ae28f4970bf3a6f8d
Nevilrv <nevilrv@gmail.com>
2025-11-18 17:53:07 +0530

    add new chnags

--- src/Components/Pages/AppRedirect.js
+++ src/Components/Pages/AppRedirect.js
@@ -1,27 +1,30 @@
 import { useEffect } from "react";
 
 export default function AppRedirect() {
-  useEffect(() => {
-    const ua = navigator.userAgent.toLowerCase();
+    useEffect(() => {
+        const ua = navigator.userAgent.toLowerCase();
 
-    const androidLink =
-      "https://play.google.com/store/apps/details?id=com.talkangels.app";
+        const androidLink =
+            "https://play.google.com/store/apps/details?id=com.talkangels.app";
 
-    // Try opening app first
-    window.location.href = "talkangels://open";
+        const cleanedPath = path.replace(/^\//, "");
+        // Try opening app first
+        window.location.href = `intent://${cleanedPath}#Intent;scheme=https;package=com.talkangels.app;S.browser_fallback_url=" +
+            encodeURIComponent(https://play.google.com/store/apps/details?id=com.talkangels.app) +
+            ";end`;
 
-    // If app is not installed → fallback after 1500ms
-    setTimeout(() => {
-      if (ua.includes("android")) {
-        window.location.replace(androidLink);
-      }
-    }, 1500);
-  }, []);
+        // If app is not installed → fallback after 1500ms
+        setTimeout(() => {
+            if (ua.includes("android")) {
+                window.location.replace(androidLink);
+            }
+        }, 1000);
+    }, []);
 
-  return (
-    <div style={{ padding: 50, textAlign: "center" }}>
-      <h2>Opening TalkAngels App...</h2>
-      <p>If nothing happens, you will be redirected shortly.</p>
-    </div>
-  );
+    return (
+        <div style={{ padding: 50, textAlign: "center" }}>
+            <h2>Opening TalkAngels App...</h2>
+            <p>If nothing happens, you will be redirected shortly.</p>
+        </div>
+    );
 }
