a32d5d3dd635afe9aacb6945a9a56a554892f4d3
Nevilrv <nevilrv@gmail.com>
2025-11-21 10:07:26 +0530

    add test creds

--- src/Components/Pages/payment/Index.js
+++ src/Components/Pages/payment/Index.js
@@ -11,7 +11,7 @@ const Index = () => {
 
   let insitialzeSDK = async function () {
     cashfree = await load({
-      mode: "production",
+      mode: "sandbox",
     });
   };
   insitialzeSDK();
@@ -26,7 +26,7 @@ const Index = () => {
     } catch (error) {
       toast.error("Payment failed, please try again");
       // navigate(Routing.Initial);
-       navigate('/', { replace: true });
+      navigate('/', { replace: true });
     }
   };
 
@@ -39,13 +39,10 @@ const Index = () => {
       const result = await AddBallenceAPI(body, user_details?.token);
       if (result.status === 200) {
         toast.success(result?.message);
-        // navigate(Routing.Initial);
-         navigate('/', { replace: true });
+        window.location = 'intent://open#Intent;scheme=https;package=com.talkangels.pro;S.browser_fallback_url=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dcom.talkangels.pro;end'
       }
     } catch (error) {
       toast.error("Payment failed, please try again");
-      // navigate(Routing.Initial);
-       navigate('/', { replace: true });
     }
   };
 
@@ -64,7 +61,7 @@ const Index = () => {
     } catch (error) {
       toast.error("Payment failed, please try again");
       // navigate(Routing.Initial);
-       navigate('/', { replace: true });
+      navigate('/', { replace: true });
     }
   };
 
@@ -74,7 +71,7 @@ const Index = () => {
 
   return (
     <>
-     <div className="card"></div>
+      <div className="card"></div>
     </>
   );
 };
