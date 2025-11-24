5e4cd8abaeafab0c74a876947cb54c706a6110d0
Nevilrv <nevilrv@gmail.com>
2025-11-24 12:22:31 +0530

    add new chnages

--- src/Components/Pages/payment/Index.js
+++ src/Components/Pages/payment/Index.js
@@ -39,7 +39,7 @@ const Index = () => {
       const result = await AddBallenceAPI(body, user_details?.token);
       if (result.status === 200) {
         toast.success(result?.message);
-        window.location = 'intent://profile#Intent;scheme=talkangels;package=com.talkangels.pro;end';
+        window.location = 'intent://profile#Intent;scheme=https;package=com.talkangels.pro;end';
       }
     } catch (error) {
       toast.error("Payment failed, please try again");
